cc49b7ce7de43e3dbaba978c48cb71a4131c2906
lsamon <louis.amon@amazutech.net>
2016-09-21 10:58:46 +1000

    Updated version of Tic Tac Toe

--- js/tictactoe-ui.js
+++ js/tictactoe-ui.js
@@ -1,24 +1,134 @@
 $(document).ready(function () {
-  console.log('Ready');
-  var playerTurn = true;
+  //Setup global Variables
+  var gameOver = false;
+  var totalMoves = 0;
+  var xpos = 0;
+  var ypos = 0;
+  $('.player-win-count.one').html(game.player1.name + ': ' + game.player1.winCount);
+  $('.player-win-count.two').html(game.player2.name + ': ' + game.player2.winCount);
+  $('span').html(game.counter);
+
+  //This function is used to update the UI eventime a DIV is clicked
+  var updateUI = function(xpos, ypos) {
+      if(grid[xpos][ypos] === game.player1.sign) {
+        var img = $('<img src="images/x.png">');
+        var $div = $('div').find("[xpos='" + xpos + "'][ypos='" + ypos + "']");
+        $div.append(img);
+      }
+      if(grid[xpos][ypos] === game.player2.sign) {
+        var img = $('<img src="images/o.png">');
+        var $div = $('div').find("[xpos='" + xpos + "'][ypos='" + ypos + "']");
+        $div.append(img);
+      }
+    };
+    
+    //This function is used to clear the board
+    var clearBoard = function() {
+      gridReset();
+      game.player1.winner = false;
+      game.player2.winner = false;
+      game.player1.turn = true;
+      game.player1.moves = 0;
+      game.player2.moves = 0;
+      gameOver = false;
+      $('.square > img').remove(); //clear squares
+      $('#winner').html(''); //clear winner div
+      $('#display-turn').html(''); //clear turns div
+      $('#game-board').css({"opacity":"1"}); // reset opacity to full
+    };
+
+// This is a function that reacts to when a square gets clicked
   $('.square').on('click', function() {
-    var xpos = $(this).attr('xpos');
-    var ypos = $(this).attr('ypos');
-    console.log(xpos);
-    console.log(ypos);
+
+    //Return early if game over is true
+    if (gameOver){
+      $('span').html(game.counter);
+      return;
+    }
+
+    xpos = $(this).attr('xpos');
+    ypos = $(this).attr('ypos');
     $(this).html('');
 
-    if(playerTurn) {
-      var img = $('<img src="images/x.png">');
-      $(this).append(img);
-      playerTurn = false;
-    }
-    else {
-      var img = $('<img src="images/o.png">');
-      $(this).append(img);
-      playerTurn = true;
-    }
-    play(xpos, ypos);
+//This just adds a message once the game is over
+    var winnerMessage = function(message) {
+      game.counter++;
+      $('span').html(game.counter);
+      $('#winner').html(message);
+      $('#winner').hide().fadeIn(2000);
+      $('#game-board').css({"opacity":"0.3"});
+      $('#display-turn').html('');
+      gameOver = true;
+    };
+
+    //Determine Player's turn function
+    var playerTurn = function () {
+      if(game.player1.turn) {
+        $('#display-turn').html(game.player1.name +"'s turn ");
+        console.log('Player 1 turn: ',game.player1.turn);
+      }
+      else {
+        $('#display-turn').html(game.player2.name +"'s turn ");
+        console.log('Player 2 turn: ',game.player2.turn);
+      }
+    };
+
+    //Determine winner by checking the current value of winner from player object
+    var determineWinner = function () {
+      if(game.player1.winner) {
+        winnerMessage(game.player1.name + ' Wins');
+        game.player1.winCount ++;
+        $('.player-win-count.one').html(game.player1.name + ': ' + game.player1.winCount);
+      }
+      else if(game.player2.winner) {
+        winnerMessage(game.player2.name + ' Wins');
+        game.player2.winCount ++;
+        $('.player-win-count.two').html(game.player2.name + ': ' + game.player2.winCount);
+      }
+      else if(totalMoves === 9) {
+        winnerMessage('Draw, Try Again');
+      }
+    };
+    // End Determine winner
+
+// Here we run the functions that play the game determine playerturn, determine winner and also updating the UI
+    playerTurn();
+    game.play(xpos, ypos);
+    playerTurn();
+    game.win();
+    determineWinner();
+    updateUI(xpos, ypos);
+    totalMoves = game.player1.moves + game.player2.moves;
+
+  });
+
+  // Start Set Players Names and update screen accordingly
+  $('button.one').on('click', function(){
+    game.player1.name = $('.user.one').val();
+    $('.player-win-count.one').html(game.player1.name + ': ' + game.player1.winCount);
+    updateUI(xpos, ypos);
+    $('.user.one').val('');
+  });
+
+  $('button.two').on('click', function(){
+    game.player2.name = $('.user.two').val();
+    $('.player-win-count.two').html(game.player2.name + ': ' + game.player2.winCount);
+    updateUI(xpos, ypos);
+    $('.user.two').val('');
+  });
+  //end Set player names
+
+//This section will clear up the game board and the winner variable
+  $('.reset').on('click', clearBoard);
+//This section has the function to reset the whole game with the scores
+  $('.reset-game').on('click', function(){
+    clearBoard();
+    game.player1.winCount = 0;
+    game.player2.winCount = 0;
+    game.counter = 0
+    $('.player-win-count.one').html(game.player1.name + ': ' + game.player1.winCount);
+    $('.player-win-count.two').html(game.player2.name + ': ' + game.player2.winCount);
+    $('span').html(game.counter);
   });
 
 });
--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,66 +1,97 @@
-//fill grid with blanks
-// replace the empty character on the grid with X or 0
+var grid = []; // Define empty grid
 
-//create grid
-
-var grid = [
+// function to reset the grid evertime the game is over or at the start of a game
+var gridReset = function() {
+  grid = [
       ['*', '*', '*'],
       ['*', '*', '*'],
       ['*', '*', '*']
     ];
-
-  // var drawGrid = function () {
-  //   console.table(grid);
-  //   return grid;
-  // }
-
-var playerTurn = true;
-var play = function (x,y) {
-  var location = grid[x][y];
-  if(location === '*' && playerTurn === true) {
-    grid[x][y] = 'X';
-    playerTurn = false;
-  }
-  else {
-    grid[x][y] = 'O';
-    playerTurn = true;
-  }
- // for(var i = 0; i < grid.length; i++) {
- //   for(var j = 0; j < grid[i].length; j++) {
- //     var coordinates = grid[i][j];
- //     var location = grid[x][y];
- //     if(coordinates === location && coordinates === '*') {
- //       grid[i][j] = 'X';
- //     }
- //   }
- // }
- console.table(grid);
 };
 
-var rowWin = function() {
-  var rowToBeChecked = '';
-  for(var i = 0; i < grid.length; i++) {
-    var row = grid[i];
-    var rowStr = row.join('');
-    for(var j = 0; j < grid[i].length; j++) {
-      rowToBeChecked += grid[i][j];
-      if(j === row.length - 1) {
-        if(rowStr === rowToBeChecked && rowStr !== '***') {
-          console.log('winner');
-        }
+gridReset();
+
+// Creating game structure
+var game = {
+  player1: {
+    name: 'Player1',
+    turn: true,
+    moves: 0,
+    sign: 'X',
+    win: 'XXX',
+    winner: false,
+    winCount: 0
+  },
+  player2: {
+    name: 'Player2',
+    turn: true,
+    moves: 0,
+    sign: 'O',
+    win: 'OOO',
+    winner: false,
+    winCount: 0
+  },
+  counter: 0,
+  play: function(x, y) {
+    if(this.player1.turn) {
+      if(grid[x][y] === '*' || grid[x][y] !== this.player2.sign) {
+        grid[x][y] = this.player1.sign;
+        this.player1.turn = false;
+        this.player1.moves ++;
+        this.player2.turn = true;
       }
     }
-  }
-}
-
-rowWin();
-
-var player = function (name,score) {
-  return {
-    name: name,
-    score: score,
-    win: function () {
-
+    else {
+      if(grid[x][y] === '*' || grid[x][y] !== this.player1.sign) {
+        grid[x][y] = this.player2.sign;
+        this.player2.turn = false;
+        this.player2.moves ++;
+        this.player1.turn = true;
+      }
     }
+    console.table(grid);
+  },
+  winRow: function() {
+    for(var i = 0; i < grid.length; i++) {
+      var row = grid[i];
+      var rowStr = row.join('');
+      if(game.player1.win === rowStr) {
+        game.player1.winner = true;
+      }
+      if(game.player2.win === rowStr) {
+        game.player2.winner = true;
+      }
+    }
+  },
+  winCol: function() {
+      for(var i = 0; i < grid.length; i++) {
+        var row = grid[i];
+        var col = [];
+        for(var j = 0; j < row.length; j++) {
+          col.push(grid[j][i]);
+        }
+        var colStr = col.join('');
+        if(game.player1.win === colStr) {
+          game.player1.winner = true;
+        }
+        if(game.player2.win === colStr) {
+          game.player2.winner = true;
+        }
+      }
+  },
+  winDiag: function() {
+    var pattern1 = grid[0][0] + grid[1][1] + grid[2][2];
+    var pattern2 = grid[0][2] + grid[1][1] + grid[2][0];
+    if(game.player1.win === pattern1 || game.player1.win === pattern2) {
+      game.player1.winner = true;
+    }
+    if(game.player2.win === pattern1 || game.player2.win === pattern2) {
+      game.player2.winner = true;
+    }
+  },
+  win: function() {
+    this.winRow();
+    this.winCol();
+    this.winDiag();
   }
 };
